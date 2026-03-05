946a08392d5f59572948308212c6cc6925df5f07
eltuzk <phanhien261106@gmail.com>
2026-03-05 09:15:43 +0700

    Initial commit

---
+++ src/main/java/com/airlinebooking/AirlineBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.airlinebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineBookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airlinebooking/AirlineBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlinebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
