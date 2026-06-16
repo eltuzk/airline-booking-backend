3839af808ae01a78199cd71546343acef59f009f
Nhat Minh <nguyenhuunhatminh426@gmail.com>
2026-06-16 16:11:02 +0700

    feat: create passengerTicketEntity

--- src/main/java/com/airlinebooking/booking/entity/PassengerTicketEntity.java
+++ src/main/java/com/airlinebooking/booking/entity/PassengerTicketEntity.java
@@ -1,4 +1,38 @@
 package com.airlinebooking.booking.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "passenger_tickets")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PassengerTicketEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer ticketId;
+
+    private BigDecimal ticketPrice;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booking_id")
+    private BookingEntity booking;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "passenger_id")
+    private PassengerEntity passenger;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seat_id", unique = true)
+    private SeatEntity seat;
+
 }
