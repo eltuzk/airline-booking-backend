e91631a15014443e0195d02ee3b68ed8dd7117f4
Nhat Minh <nguyenhuunhatminh426@gmail.com>
2026-06-16 16:11:27 +0700

    feat: create passengerEntirty

--- src/main/java/com/airlinebooking/booking/entity/PassengerEntity.java
+++ src/main/java/com/airlinebooking/booking/entity/PassengerEntity.java
@@ -1,4 +1,40 @@
 package com.airlinebooking.booking.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity(name = "passengers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PassengerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer passengerId;
+
+    private String fullName;
+
+    private LocalDate dateOfBirth;
+
+    private String gender; // MALE, FEMALE, OTHER
+
+    private String passengerType; // ADULT, CHILD, INFANT
+
+    private String passportNumber;
+
+    private String identityCard;
+
+    private Integer countryId;
+
+    @OneToMany(mappedBy = "passenger")
+    private List<PassengerTicketEntity> passengerTicketEntityList;
+
 }
