5e0bb91116b92b58e40a5e3868ad603d0b375f9a
Nhat Minh <nguyenhuunhatminh426@gmail.com>
2026-06-16 10:54:20 +0700

    feat: add a new controller lockSeat

--- src/main/java/com/airlinebooking/booking/controller/SeatController.java
+++ src/main/java/com/airlinebooking/booking/controller/SeatController.java
@@ -1,4 +1,46 @@
 package com.airlinebooking.booking.controller;
 
+import com.airlinebooking.booking.payload.request.LockSeatRequest;
+import com.airlinebooking.booking.payload.response.BaseResponse;
+import com.airlinebooking.booking.service.RedisService;
+import com.airlinebooking.booking.service.imp.RedisServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/seat")
 public class SeatController {
+
+    @Autowired
+    private RedisService redisService;
+
+    @PostMapping("/lock")
+    public ResponseEntity<?> lockSeat(@RequestBody LockSeatRequest lockSeatRequest){
+        Integer currentUserId = 1;      // tạm thời set cứng id của khách hàng vì chưa làm xong
+        boolean isLocked = redisService.lockSeat(lockSeatRequest.getFlightId(), currentUserId, lockSeatRequest.getSeatNumber());
+
+
+
+
+        BaseResponse baseResponse = new BaseResponse();
+
+        if(isLocked){
+            baseResponse.setCode(200);
+            baseResponse.setMessage("Bạn đã chọn ghế, có 15 phút để thao tác");
+            return ResponseEntity.ok(baseResponse);
+        } else {
+            baseResponse.setCode(400);
+            baseResponse.setMessage("Xin lỗi, ghế này đã có người chọn");
+            return ResponseEntity.badRequest().body(baseResponse);
+        }
+
+
+
+    }
+
+
 }
--- src/main/java/com/airlinebooking/booking/payload/request/LockSeatRequest.java
+++ src/main/java/com/airlinebooking/booking/payload/request/LockSeatRequest.java
@@ -1,4 +1,12 @@
 package com.airlinebooking.booking.payload.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LockSeatRequest {
+
+    private Integer flightId;
+    private String seatNumber;
 }
